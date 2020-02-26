3b8e3b7e21b84e2dc77c617abbfcc2aab034d71a
jiho lee <jiholee@kpu.ac.kr>
2020-02-26 19:39:51 +0900

    done

---
+++ index.js
@@ -0,0 +1,37 @@
+const button = document.getElementById("convert");
+const inputArea = document.getElementById("inputArea");
+
+function changeValue(id, ctx) {
+    const elem = document.getElementById(id);
+    elem.innerText = ctx;
+}
+
+function changeInnerHtml(id, html) {
+    const elem = document.getElementById(id);
+    elem.innerHTML = html;
+}
+
+function show() {
+    document.getElementById("output")
+        .style
+        .display = "grid";
+}
+
+function hide() {
+    document.getElementById("output")
+        .style
+        .display = "none";
+}
+
+function parse() {
+    const val = inputArea.value;
+    const res = val.replace("<iframe", "<embed").replace("</iframe>", "</embed>");
+    return res;
+}
+
+button.addEventListener("click", function() {
+    const res = parse();
+    changeValue("convertSpan", res);
+    changeInnerHtml("previewDiv", res);
+    show();
+});
